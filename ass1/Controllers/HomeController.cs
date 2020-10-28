29e1eaf61ff4b9295e9bec92cc48a0538ee1055c
vanhngyen <53243467+vanhngyen@users.noreply.github.com>
2020-10-28 09:04:14 +0700

    Add project files.

---
+++ ass1/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using ass1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ass1.Controllers
+{
+    public class HomeController : Controller
+    {
+        DealAndAccountContext db = new DealAndAccountContext();
+        // GET: Home
+        public ActionResult Index()
+        {
+            var data = db.Accounts.ToList();
+            return View(data);
+        }
+    }
+}
---
+++ ass1/Models/DealAndAccountContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ass1.Models
+{
+    public class DealAndAccountContext:DbContext
+    {
+        public DealAndAccountContext() : base("name=Asignment1Context")
+        {
+
+        }
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Deal> Deals { get; set; }
+    }
+}
